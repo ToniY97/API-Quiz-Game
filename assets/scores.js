4d298ad1ba304d026310416f00fc5abc7c2598bf
Toni97 <ayovcheva75@gmail.com>
2024-01-02 08:52:00 +0000

    high scores not displaying

---
+++ assets/scores.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Retrieve scores from localStorage
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    // Get the elemnt where you want to display high scores
+    const highScoresList = document.getElementById("highscores");
+
+    //Display each high score in the list
+    highScores.array.forEach((score, index) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${index + 1}. ${score.initials}: ${score.score}`;
+      highScoresList.appendChild(listItem);  
+    });
+})
